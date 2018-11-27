8309829a279162a8abd9ee0530c7525d04659a35
juanspada <spadajuan94@gmail.com>
2018-11-27 18:47:14 -0300

    img navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,7 @@
                             @else
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <img class="img-nav" src="/storage/avatars/{{Auth::user()->avatar}}">
                                         {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
                                     
